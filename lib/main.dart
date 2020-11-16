1eabc1de686032731937cc2247069d47ff64f70d
Ayush Dubey <ayushdubey70@gmail.com>
2020-11-16 20:55:20 +0530

    bmi: use darkTheme
    * it has most of the stuff we need in our app
    
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -6,13 +6,13 @@ class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
+      theme: ThemeData.dark().copyWith(
         primaryColor: Color(0xFF0A0E21),
         scaffoldBackgroundColor: Color(0xFF0A0E21),
         textTheme: TextTheme(
           bodyText2: TextStyle(
-                color: Colors.white,
-            ),
+            color: Colors.white,
+          ),
         ),
       ),
       home: InputPage(),
