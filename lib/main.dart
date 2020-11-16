8828eeb78b87880b29490cb9ca5b21f05a8eef12
Ayush Dubey <ayushdubey70@gmail.com>
2020-11-16 20:46:39 +0530

    bmi: add primaryColor and scaffoldBackgroundColor
    * Also make the text color to white so that it is readable
    
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,15 @@ class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Color(0xFF0A0E21),
+        scaffoldBackgroundColor: Color(0xFF0A0E21),
+        textTheme: TextTheme(
+          body1: TextStyle(
+                color: Colors.white,
+            ),
+        ),
+      ),
       home: InputPage(),
     );
   }
