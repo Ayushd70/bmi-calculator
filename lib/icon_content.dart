b69ab74054354001a34a9e8c3ccae691842786f6
Ayush Dubey <ayushdubey70@gmail.com>
2021-01-27 14:11:48 +0530

    bmi: icon_content: don't hardcode the Text Label
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

--- lib/icon_content.dart
+++ lib/icon_content.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 
+const labelTextStyle = TextStyle(
+  fontSize: 18.0,
+  color: Color(0xFF8D8E98),
+);
+
 class IconContent extends StatelessWidget {
   IconContent({@required this.icon, @required this.label});
 
@@ -20,10 +25,7 @@ class IconContent extends StatelessWidget {
         ),
         Text(
           label,
-          style: TextStyle(
-            fontSize: 18.0,
-            color: Color(0xFF8D8E98),
-          ),
+          style: labelTextStyle,
         ),
       ],
     );
