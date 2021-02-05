02b387ddbb6051b6d75893b13233fbafd6d9e617
Ayush Dubey <ayushdubey70@gmail.com>
2021-02-05 20:41:31 +0530

    bmi: input_page: use Enum class to select Gender
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -9,6 +9,11 @@ const activeCardColour = Color(0xFF1D1E33);
 const inactiveCardColour = Color(0xFF111328);
 const bottomContainerColour = Color(0xFFEB1555);
 
+enum Gender {
+  male,
+  female,
+}
+
 class InputPage extends StatefulWidget {
   @override
   _InputPageState createState() => _InputPageState();
@@ -19,16 +24,16 @@ class _InputPageState extends State<InputPage> {
   Color femaleCardColour = inactiveCardColour;
 
   //1 = male, 2 = female
-  void updateColour(int gender) {
+  void updateColour(Gender selectedGender) {
     // male card pressed
-    if (gender == 1) {
+    if (selectedGender == Gender.male) {
       if (maleCardColour == inactiveCardColour) {
         maleCardColour = activeCardColour;
       } else {
         maleCardColour = inactiveCardColour;
       }
     }
-    if (gender == 2) {
+    if (selectedGender == Gender.female) {
       if (femaleCardColour == inactiveCardColour) {
         femaleCardColour = activeCardColour;
       } else {
@@ -52,7 +57,7 @@ class _InputPageState extends State<InputPage> {
                   child: GestureDetector(
                     onTap: () {
                       setState(() {
-                        updateColour(1);
+                        updateColour(Gender.male);
                       });
                     },
                     child: ReusableCard(
@@ -68,7 +73,7 @@ class _InputPageState extends State<InputPage> {
                   child: GestureDetector(
                     onTap: () {
                       setState(() {
-                        updateColour(2);
+                        updateColour(Gender.female);
                       });
                     },
                     child: ReusableCard(
