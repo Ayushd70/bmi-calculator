0f85f62c2ca8c002997a25abbc4403eea32df26a
Ayush Dubey <ayushdubey70@gmail.com>
2021-02-06 14:31:19 +0530

    bmi: align the cm text with the baseline
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -75,6 +75,8 @@ class _InputPageState extends State<InputPage> {
                   ),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.baseline,
+                    textBaseline: TextBaseline.alphabetic,
                     children: [
                       Text(
                         '180',
